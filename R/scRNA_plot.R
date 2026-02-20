b7c1af5672d545b0d19c2ad6e08997710409c239
YuhengTao949 <YuhengTao290037@gmail.com>
2026-02-20 09:48:38 +0800

    Fix a bug in function ThreshPlot

--- R/scRNA_plot.R
+++ R/scRNA_plot.R
@@ -60,58 +60,68 @@ ThreshPlot <- function(seu,
 
   plist <- lapply(feature, function(x){
 
-    plotdata <- cbind(seu@meta.data,
-                      Embeddings(seu, reduction),
-                      FetchData(seu, var = x))
+    meta <- seu@meta.data
+    embed <- Embeddings(seu, reduction)
+    feat_data <- FetchData(seu, var = x)
+    
+    meta <- meta[, !colnames(meta) %in% colnames(feat_data), drop = FALSE]
+    plotdata <- cbind(meta, embed, feat_data)
 
     mean.val <- mean(plotdata[, x])
     sd.val <- sd(plotdata[, x])
     thresh <- if (is.null(threshold)) round(mean.val + 2*sd.val, 2) else threshold
+
     plotdata$color_val <- ifelse(plotdata[[x]] > thresh, plotdata[[x]], NA)
     plotdata <- plotdata[order(plotdata[[x]], decreasing = FALSE), ]
 
-    x_col <- colnames(Embeddings(seu, reduction))[dims[1]]
-    y_col <- colnames(Embeddings(seu, reduction))[dims[2]]
+    x_col <- colnames(embed)[dims[1]]
+    y_col <- colnames(embed)[dims[2]]
 
-    p <- ggplot(plotdata) + geom_point(
-      aes(x = .data[[x_col]],
-          y = .data[[y_col]],
-          color = color_val),
-      size = dot.size,
-      stroke = 0,
-      shape = 16,
-      alpha = alpha
-    ) +
+    p <- ggplot(plotdata) + 
+      geom_point(
+        aes(x = .data[[x_col]],
+            y = .data[[y_col]],
+            color = color_val),
+        size = dot.size,
+        stroke = 0,
+        shape = 16,
+        alpha = alpha
+      ) +
       labs(color = legend.title)+
       theme_void() +
       theme(
         aspect.ratio = 1,
         text = element_text(family = "Arial")
       )+
-      scale_color_gradientn(colours = color,
-                            na.value = "grey80",
-                            limits = c(thresh, max(plotdata[[x]], na.rm = TRUE)),
-                            oob = scales::squish)
-      if(is.null(title)){
-        p <- p+labs(title = x)
-      }else{
-        p <- p+labs(title = title)
-      }
-      p <- p+theme(
-        plot.title = element_text(
-          size = title.size,
-          face = "bold",
-          family = "Arial",
-          color = "black",
-          hjust = 0.5,
-          vjust = 1
-        ))
-
+      scale_color_gradientn(
+        colours = color,
+        na.value = "grey80",
+        limits = c(thresh, max(plotdata[[x]], na.rm = TRUE)),
+        oob = scales::squish
+      )
+    
+    if(is.null(title)){
+      p <- p + labs(title = x)
+    } else {
+      p <- p + labs(title = title)
+    }
+    
+    p <- p + theme(
+      plot.title = element_text(
+        size = title.size,
+        face = "bold",
+        family = "Arial",
+        color = "black",
+        hjust = 0.5,
+        vjust = 1
+      )
+    )
+    return(p)
   })
 
   p <- plist %>%
-    patchwork::wrap_plots(ncol = ncol)+
-    patchwork::plot_layout(guides = 'collect')&
+    patchwork::wrap_plots(ncol = ncol) +
+    patchwork::plot_layout(guides = 'collect') &
     ggplot2::theme(
       legend.position = legend.position,
       legend.title = element_text(size = legend.title.size,
@@ -124,7 +134,48 @@ ThreshPlot <- function(seu,
       legend.box.margin = margin(10, 0, 0, 0)
     )
 
-
   return(p)
+}
 
+
+ViolinPlot <- function(seu = NULL, 
+                       metacol = NULL, 
+                       cellcol = NULL, 
+                       cellchoose = NULL, 
+                       feature = NULL,
+                       cell_filter_thres = 0){
+  
+  plotdata <- FetchData(seu, var = c(cellcol, metacol, feature))
+
+
+  plotdata <- plotdata %>% dplyr::filter(major_celltype == cellchoose & TNF > 0)
+  
+  p <- ggplot(data = plotdata, aes(x = ERScore_group,y = TNF, color = ERScore_group)) + 
+    geom_violin(trim = FALSE,position = position_dodge(0.9)) +  # size：加粗小提琴的边框,trim = FALSE：确保小提琴图的尾部不会被裁剪
+    geom_signif(comparisons = list(c("High_ERScore", "Low_ERScore")), # 添加显著性，y_position控制标记的y轴位置，textsize控制文本大小，size控制线的粗细
+                test = "t.test",colour = "black",
+                test.args = list(var.equal = TRUE, alternative = "two.sided"),
+                map_signif_level = TRUE, textsize = 6, tip_length = c(0, 0),
+                y_position = max(plotdata[,"TNF"]),
+                vjust=0
+    )+  
+    stat_summary(aes(group = ERScore_group,color = ERScore_group), position = position_dodge(0.9),
+                 width = 0.2, fun = mean, geom = "errorbar", 
+                 fun.min = function(x) quantile(x, 0.25),
+                 fun.max = function(x) quantile(x, 0.75)) + 
+    stat_summary( aes(group = ERScore_group,color = ERScore_group), position = position_dodge(0.9),
+                  width = 0.2,  show.legend = FALSE,
+                  fun = mean, geom = "crossbar") + 
+    scale_color_manual(values = c("Low_ERScore"="#667a57","High_ERScore"="#dfadab")) + 
+    scale_y_continuous(limits = c(0, max(plotdata[,"TNF"])*1.1)) + 
+    labs(title = cellchoose,
+         x = "",
+         y = paste0("TNF expression levels")) + 
+    theme_classic() + 
+    theme (legend.position = "none",
+           axis.line = element_line(color = "black"), 
+           axis.title = element_text(size = 10,,face = "bold"),
+           axis.text = element_text(size = 9),
+           plot.title = element_text(hjust = 0.5, size = 11, face = "bold"))
+  return(p)
 }
